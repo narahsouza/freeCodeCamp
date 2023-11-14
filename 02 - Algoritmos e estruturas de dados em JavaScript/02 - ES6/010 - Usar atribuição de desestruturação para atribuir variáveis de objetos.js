9efb4e60e270d59a666f91222587db8a732f3c23
narahsouza <narahnsa@gmail.com>
2023-11-14 20:49:01 -0300

    Add files

---
+++ 02 - Algoritmos e estruturas de dados em JavaScript/02 - ES6/010 - Usar atribuição de desestruturação para atribuir variáveis de objetos.js
@@ -0,0 +1,12 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Altere apenas o código abaixo desta linha
+
+const { today: highToday } = HIGH_TEMPERATURES;
+const { tomorrow: highTomorrow } = HIGH_TEMPERATURES; 
+
+// Altere apenas o código acima desta linha
---
+++ 02 - Algoritmos e estruturas de dados em JavaScript/02 - ES6/011 - Usar atribuição de desestruturação para atribuir variáveis de objetos aninhados.js
@@ -0,0 +1,14 @@
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+// Altere apenas o código abaixo desta linha
+
+// const lowToday = LOCAL_FORECAST.today.low;
+// const highToday = LOCAL_FORECAST.today.high;
+
+const { today: { low: lowToday, high: highToday }} = LOCAL_FORECAST;
+
+// Altere apenas o código acima desta linha
